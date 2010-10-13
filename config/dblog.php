cbacc5911376dd84ac9b286e6db4943dfa69634e
Bastian Bräu <bb@bftmedia.de>
2010-10-13 18:00:57 +0200

    Add missing config file

---
+++ config/dblog.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+return array(
+
+	'table' => 'logs',
+
+	'pagination' => array(
+		'items_per_page' => 20,
+		'view' => 'pagination/floating',
+	),
+
+);
